bbae2559181e47914b1dfadf130c16ee869f9467
Gareth Mc Cumskey <gareth@mccumskey.com>
2020-11-20 15:27:03 +0200

    change

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Changed',
+        message: 'changed again',
         input: event,
       },
       null,
